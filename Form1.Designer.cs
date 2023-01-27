00b98e0c7f6b068b5ab99d354fb87efddf1de021
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2023-01-28 05:55:36 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace TextToSpeechApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDialog = new System.Windows.Forms.TextBox();
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDialog
+            // 
+            this.txtDialog.Location = new System.Drawing.Point(27, 36);
+            this.txtDialog.Multiline = true;
+            this.txtDialog.Name = "txtDialog";
+            this.txtDialog.Size = new System.Drawing.Size(412, 307);
+            this.txtDialog.TabIndex = 0;
+            this.txtDialog.TextChanged += new System.EventHandler(this.txtDialog_TextChanged);
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(89, 364);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnRecord.TabIndex = 1;
+            this.btnRecord.Text = "Preview";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog_FileOk);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(263, 364);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(182, 364);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 480);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnRecord);
+            this.Controls.Add(this.txtDialog);
+            this.Name = "Form1";
+            this.Text = "TextConverter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDialog;
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace TextToSpeechApp
+{
+    public partial class Form1 : Form
+    {
+        private SpeechSynthesizer synthesizer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            synthesizer = new SpeechSynthesizer();
+
+        }
+
+
+        private void txtDialog_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+           
+            synthesizer.SpeakAsync(txtDialog.Text);
+
+        }
+
+        private void saveFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {          
+
+            saveFileDialog.Filter = "WAV file|*.wav";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                synthesizer.SetOutputToWaveFile(saveFileDialog.FileName);
+                synthesizer.Speak(txtDialog.Text);
+                synthesizer.SetOutputToDefaultAudioDevice();
+            }
+        }
+
+
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+
+            synthesizer.SpeakAsyncCancelAll();
+            //if (synthesizer.State == SynthesizerState.Speaking || synthesizer.State == SynthesizerState.Speaking)
+            //{
+            //    synthesizer.SpeakAsyncCancel(Prompt prompt);
+            //}
+        }
+    }
+}
